bda1b795f083fda703e12f09f9091f3318211e17
jchmyz <jchmyz@wellesley.edu>
2018-10-27 13:33:24 -0700

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+        name='DoublyLabeledWater',
+        version='0.1.0',
+        author='Julia Chmyz',
+        author_email='julia@chmyz.com',
+        packages=['dlw'],
+        scripts=[],
+        url='http://pypi.python.org/pypi/TowelStuff/',
+        license='LICENSE.txt',
+        description='add me later',
+        long_description=open('README.md').read(),
+        install_requires=[
+            "numpy",
+        ],
+)
