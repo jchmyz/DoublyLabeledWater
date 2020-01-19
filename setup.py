76d8f3c166fdbf73f87affd2a82198a7d3a7577d
jchmyz <jchmyz@wellesley.edu>
2020-01-18 18:11:09 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
         name='dlw',
-        version='0.0.7',
+        version='0.0.8',
         author='Julia Chmyz',
         author_email='julia@chmyz.com',
         packages=setuptools.find_packages(),
