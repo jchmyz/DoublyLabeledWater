edbb4bae35cca48ed374ca598ce481d687082989
jchmyz <jchmyz@wellesley.edu>
2020-07-15 16:14:24 -0700

    bump to 0.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
         name='dlw',
-        version='0.0.8',
+        version='0.0.9',
         author='Julia Chmyz',
         author_email='julia@chmyz.com',
         packages=setuptools.find_packages(),
@@ -11,6 +11,7 @@ setuptools.setup(
         license='LICENSE.txt',
         description='Open source web app to conduct doubly labeled water study calculations.',
         long_description=open('README.md').read(),
+        long_description_content_type="text/markdown",
         install_requires=[
             "numpy",
             "scipy",
